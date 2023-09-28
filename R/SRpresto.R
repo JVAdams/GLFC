1242e9ef7cb4b6e50ee1b7a500f4d8ec46c49ca3
Ted Treska <TTreska@glfc.org>
2023-09-28 12:58:06 -0400

    Added some comments to explain what is going on
    No code changes

--- R/SRpresto.R
+++ R/SRpresto.R
@@ -249,7 +249,7 @@ SRpresto <- function(FOLDER, INDEX.LAKE, INDEX.STREAM, MAXLARVAE,
 
 
 
-  #### Targets ####
+  #### Targets - this makes the StatusTargets.csv file ####
   sptargyrz <- list(1994:1998, 1988:1992, 1989:1993, 1991:1995, 1993:1997)
   sptargyrz <- list(1994:1998, 1995:1999, 1989:1993, 1991:1995, 1993:1997)
   TARGET <- data.frame(lake=1:5, wound.target=rep(c(5, 2), c(4, 1)),
@@ -314,7 +314,7 @@ SRpresto <- function(FOLDER, INDEX.LAKE, INDEX.STREAM, MAXLARVAE,
 
 
 
-  #### All lakewide data combined ####
+  #### All lakewide data combined, this makes the StatusMetrics.csv file ####
   a <- merge(adult, control, all=TRUE)
   ALL <- merge(a, trout, all=TRUE)
   ALL$treat.year <- ALL$spawner.year - 2
